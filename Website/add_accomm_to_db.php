bf6368c6b1c66b1146901d829e8c9fd882826112
Burak Mandira <burakmandira@gmail.com>
2016-12-30 06:21:29 +0300

    Added two other php pages

---
+++ Website/add_accomm_to_db.php
@@ -0,0 +1,164 @@
+<?php
+	include_once 'dbconnect.php';
+	session_start();
+	ob_start();
+
+	$address = $_POST['address'];
+	$city = $_POST['city'];
+	$district = $_POST['district-list'];
+	$start_date = $_POST['datetimepicker6'];
+	$end_date = $_POST['datetimepicker7'];
+	$num_of_people = $_POST['num_of_people'];
+	$type = $_POST['accom_type'];
+	$price = $_POST['price'];
+	$amenity = [];
+
+	if (isset($_POST['wifi'])){
+		$wifi = 1;
+	}
+	else {
+		$wifi = 0;
+	}
+	$amenity[] = $wifi;
+	if (isset($_POST['ethernet'])){
+		$ethernet = 1;
+	}
+	else {
+		$ethernet = 0;
+	}
+	$amenity[] = $ethernet;
+	if (isset($_POST['tv'])) {
+		$tv = 1;
+	}
+	else {
+		$tv = 0;
+	}
+	$amenity[] = $tv;
+	if (isset($_POST['cable'])) {
+		$cable = 1;
+	}
+	else {
+		$cable = 0;
+	}
+	$amenity[] = $cable;
+	if (isset($_POST['kitchen'])) {
+		$kitchen = 1;
+	}
+	else {
+		$kitchen = 0;
+	}
+	$amenity[] = $kitchen;
+	if (isset($_POST['washing'])) {
+		$washing = 1;
+	}
+	else {
+		$washing = 0;
+	}
+	$amenity[] = $washing;
+	if (isset($_POST['dryer'])) {
+		$dryer = 1;
+	}
+	else {
+		$dryer = 0;
+	}
+	$amenity[] = $dryer;
+	if (isset($_POST['bathtub'])) {
+		$bathtub = 1;
+	}
+	else {
+		$bathtub = 0;
+	}
+	$amenity[] = $bathtub;
+	if (isset($_POST['hangers'])) {
+		$hangers = 1;
+	}
+	else {
+		$hangers = 0;
+	}
+	$amenity[] = $hangers;
+	if (isset($_POST['iron'])) {
+		$iron = 1;
+	}
+	else {
+		$iron = 0;
+	}
+	$amenity[] = $iron;
+	if (isset($_POST['parking'])) {
+		$parking = 1;
+	}
+	else {
+		$parking = 0;
+	}
+	$amenity[] = $parking;
+	$ainput = [];
+	
+	for ($i = 0; $i < 11; $i = $i+1 ) {
+		if ($amenity[$i] === 1){
+			$ainput[] = $i+1;
+		}
+	}
+	#echo $address ." | ". $city." | ".$district." | ".$start_date." | ".$end_date." | ".$num_of_people." | ".$type." | ".$price."<br>";
+	#print_r($amenity);
+	#echo "<br>";
+	#print_r($ainput);
+
+	$success_msg = "Your accommodation is sucessfully added to database!";
+	$err_msg = "Error(s) occured while adding accommodation! Please try it again or contact customer service.";
+
+	$sql = "INSERT INTO `Accommodation` VALUES(NULL, $num_of_people, $type, NULL);";
+	$result = $db->query($sql);
+	if( $result == true)
+	{
+		$accom_id = $db->insert_id;
+		$country = NULL;
+		$sql = "INSERT INTO `Address` VALUES($accom_id, '$address', '$district', '$city', '$country');";
+		$result = $db->query($sql);
+		if( $result == true)
+		{	
+			foreach($ainput as $a_id)
+			{
+				$sql = "INSERT INTO `Contains` VALUES($a_id, $accom_id);";
+				$result = $db->query($sql);
+				if( $result == false)
+					showMessage( "Error is occured while adding amenities. DB is screwed up!", "add_accommodation.php");
+			}
+		
+			// needs to be retrieved from add_accommodation.php (no time!)		
+			$num_of_rooms = 3;
+			$num_of_beds = 2;
+			$num_of_wc = 2;
+
+			if( $type == 0)	// House
+				$sql = "INSERT INTO `House` VALUES($accom_id, $num_of_rooms, $num_of_wc);";
+			else // Room
+				$sql = "INSERT INTO `Room` VALUES($accom_id, $num_of_beds);";
+			$result = $db->query($sql);
+			if( $result == true)
+			{
+				$account_id = $_SESSION['user'];
+				$sql = "INSERT INTO `Offering` VALUES(NULL, $account_id, $accom_id, '$start_date', '$end_date', $price);";
+				$result = $db->query($sql);
+				if( $result == true)
+					showMessage( $success_msg, "my_offerings.php");
+				else
+					showMessage( "Error occured while adding the offering. DB is screwed up!", "add_accommodation.php");
+			}
+		}
+		else
+			showMessage( "Error occured while adding the address. DB is screwed up!", "add_accommodation.php");	
+	}
+	else
+		showMessage( $error_msg, "add_accommodation.php");
+
+	function showMessage( $message, $direction)
+	{
+		echo "<script type='text/javascript'>";
+		echo "alert('" . $message. "');";
+		#if( $direction)
+		echo 'window.location.href="' .$direction. '";';
+		#else
+		#	echo 'document.forms["post_values"].submit();';
+		echo "</script>";
+		die;
+	}
+?>
---
+++ Website/my_offerings.php
@@ -0,0 +1,151 @@
+<div class="container">
+
+	<table class="table table-bordered table-hover">
+		<thead> 
+			<tr>
+			<td>Accommodation ID</td>
+			<td>Accommodation Type</td>
+			<td>Price per Night ($)</td>
+			<td>Number of People</td>
+			<td>Address</td>
+			<td>Link</td>
+			<!--
+			<td>Number of Rooms</td>
+			<td>Number of Beds</td>
+			<td>Amenities</td>
+			<td>Number of Bathrooms</td>
+			-->
+			</tr>
+		</thead>
+		<tbody>
+
+<?php
+	include_once 'dbconnect.php';
+	session_start();
+	ob_start();
+
+	if ( !isset($_SESSION['user']))
+	{
+		header("Location: login.php");
+		die;
+	}
+	
+	// print welcome message at the right-upper corner
+	$accountID = $_SESSION['user'];
+	$sql = 'SELECT email FROM Account WHERE account_ID = ' .$accountID. ';';
+	$result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result);
+	if( $result->num_rows < 1)
+	{
+		echo $result->num_rows;
+		$message = "Invalid account credentials. Please check your email/password!";
+		echo "<script type='text/javascript'>";
+		echo "alert('" . $message. "');";
+		echo 'window.location.href="login.php";';		
+		echo "</script>";
+		die;
+	}
+	echo "<h5 align='right' style='padding-right: 10px'><a href='account.php'> Welcome " . $row['email'] . "</a></h4>";
+
+	// pull all offerings of the account holder
+	$sql = "SELECT A.accommodation_ID, A.type, O.price_per_night, O.offering_ID, A.num_of_people, D.street, D.district, D.city, D.country
+			FROM Accommodation A, Offering O, Address D
+			WHERE A.accommodation_ID = O.accommodation_ID AND A.accommodation_ID = D.accommodation_ID 
+					AND O.account_ID = $accountID;";
+	#echo $sql;
+	$result = mysqli_query($db, $sql);
+	$res_reqs = array();
+	if( $result->num_rows > 0)
+		while($row = mysqli_fetch_array($result))
+			array_push($res_reqs, $row);
+	
+	#echo "</br>";
+	#print_r($res_reqs);
+	if( count( $res_reqs) < 1)
+		echo "<h2 align='center'><b> You have no offered accommodations! </b></h2>";
+	else
+	{
+		echo "<h2 align='center'><b> Offered Accommodations </b></h2>";
+		foreach($res_reqs as $tuple)
+		{
+			echo "<tr>";
+			$id = $tuple['accommodation_ID'];
+			$type = $tuple['type'];
+			if ( $type === 0) {
+				$type2 = 'Room';
+			}
+			else {
+				$type2 = 'House';
+			}
+			$offID = $tuple['offering_ID'];
+			$price = $tuple['price_per_night'];
+			$people = $tuple['num_of_people'];
+			$street = $tuple['street'];
+			$district = $tuple['district'];
+			$city = $tuple['city'];
+			$country = $tuple['country'];
+			echo "<td>$id</td>
+				  <td>$type2</td>
+				  <td>$price</td>
+				  <td>$people</td>
+				  <td>$street, $district, $city, $country</td>
+				  <td><a href=\"info.php?id=$offID\">Details</a></td>
+				  </tr>";
+		}
+	}
+?>
+
+		</tbody>
+	</table>
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+	<link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
+
+	<title>CnC</title>
+
+</head>
+<body style="padding-top: 65px;">
+   <!-- Fixed navbar -->
+   <nav class="navbar navbar-inverse navbar-fixed-top">
+       <div class="container">
+         <div class="navbar-header">
+           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+             <span class="sr-only">Toggle navigation</span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="index.php">Crash&Sleep</a>
+     </div>
+
+     <div id="navbar" class="navbar-collapse collapse">
+       <ul class="nav navbar-nav">
+         <li><a href="index.php">Home</a></li>
+         <li><a href="#">About</a></li>
+		 <li><a href="detailed_search.php">Detailed Search</a></li>
+		 <li><a href="add_accommodation.php">Offer Accommodation</a></li>
+     </ul>
+     <ul class="nav navbar-nav navbar-right">
+         <li  class="active" ><a href="logout.php">Log out</a></li>
+
+     </ul>
+ </div>
+</div>
+</nav>
+</body>
+</html>
+<?php ob_end_flush(); ?>
